85d99cc121ec717e2199c9284fd5588fede7e242
knvc <koenvancrombrugge@outlook.com>
2023-02-25 20:08:32 +0100

    Add tab script

---
+++ tab.js
@@ -0,0 +1,30 @@
+console.log('tab.js');
+
+var currentUrl = window.location.href,
+    ticketPageUrl = {
+        'ticket_page': '.ionbiz.com/Issue/Index',
+        'kanban_page': '.ionbiz.com/IssueKanban/Index'
+    };
+
+if (currentUrl.includes(ticketPageUrl.ticket_page) || currentUrl.includes(ticketPageUrl.kanban_page)) {
+    var uniqueId = document.getElementsByClassName('uniqueId')[0];
+
+    if (uniqueId) {
+        let linkId = document.getElementById('TabGeneral_IssueDetailSection_IssueId').value,
+            linkDescription = document.getElementById('TabGeneral_IssueDetailSection_Name').value,
+            linkIndex = document.getElementById('Id').value;
+
+        let newLink = linkId + ' ' + linkDescription + ' ' + 'https://' + window.location.hostname + '/Issue/Index/' + linkIndex + ' ';
+        var data = [new ClipboardItem({ "text/plain": new Blob([newLink], { type: "text/plain" }) })];
+        navigator.clipboard.write(data).then(function () {
+            console.log("Copied to clipboard successfully!");
+        }, function () {
+            console.error("Unable to write to clipboard");
+        });
+
+    } else {
+        console.error('Could not provide a link. Check if you have clicked a ticket.')
+    }
+} else {
+    console.log('you are not on the right page')
+}
