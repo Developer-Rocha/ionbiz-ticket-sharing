69d024af1024283fc953154be439a84edacea36e
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2023-03-11 16:55:00 +0100

    Save form options in storage sync

--- src/options/options.js
+++ src/options/options.js
@@ -1,10 +1,15 @@
+var PLAIN_URL = 'plain-link';
+var URL_AND_TEXT = 'plain-link-and-text';
+var HTML_LINK = 'html-link';
+
 // Saves options to chrome.storage
 function save_options() {
-    var domain = document.getElementById('domain').value;
-    var autoCopy = document.getElementById('auto-copy').checked;
     chrome.storage.sync.set({
-        domain: domain,
-        autoCopy: autoCopy
+        'options': {
+            PLAIN_URL: document.getElementById(PLAIN_URL).checked,
+            URL_AND_TEXT: document.getElementById(URL_AND_TEXT).checked,
+            HTML_LINK: document.getElementById(HTML_LINK).checked
+        }
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -18,13 +23,10 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        domain: 'domain',
-        autoCopy: true
-    }, function (items) {
-        document.getElementById('domain').value = items.domain;
-        document.getElementById('auto-copy').checked = items.autoCopy;
+    chrome.storage.sync.get(["options"]).then((result) => {
+        document.getElementById(PLAIN_URL).checked = result.options.PLAIN_URL
+        document.getElementById(URL_AND_TEXT).checked = result.options.URL_AND_TEXT;
+        document.getElementById(HTML_LINK).checked = result.options.HTML_LINK;
     });
 }
 
