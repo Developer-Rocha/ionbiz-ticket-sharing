19494b4e6ab58820e4e58d425144ffe5a6486576
Koen Van Crombrugge <koenvancrombrugge@hotmail.com>
2023-03-19 17:50:33 +0100

    Close options window on save action

--- src/options/options.js
+++ src/options/options.js
@@ -24,10 +24,8 @@ function save_options() {
             HTML_LINK: document.getElementById(HTML_LINK).checked
         }
     }, function () {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-
+        // close window
+        window.close();
     });
 }
 
