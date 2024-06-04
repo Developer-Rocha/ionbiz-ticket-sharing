cdba740f9170649a194d70453194d034e027afbb
Fabrício Rocha <fabricio.lopesx@gmail.com>
2024-06-04 09:42:29 -0300

    Add templates

--- background.js
+++ background.js
@@ -85,13 +85,52 @@ function insertDefaultDescription(options) {
             Aarzel niet om ons te contacteren indien er nog vragen zijn.<br><br>
             Met vriendelijke groeten,<br>[naam]
         `,
-        DESCRIPTION_2: `
-            Description 2: Beste [naam],<br><br>
-            [andere_omschrijving_probleem]<br><br>
-            [andere_omschrijving_oplossing]<br><br>
-            Je kan de aanpassingen via de volgende URL testen op de {staging/productie} omgeving: {url}.<br><br>
-            Geef gerust een seintje na testing wanneer we dit mogen deployen naar productie.<br><br>
-            Aarzel niet om ons te contacteren indien er nog vragen zijn.<br><br>
+        TEMPLATE_BUGS: `
+            Beste [naam],<br><br>
+            We hebben een fix voorzien die hier gevalideerd kan worden: [link naar expliciete url]<br>
+            Je kan dit testen door<br>
+                • [stappenplan]<br>
+                • [rol waarmee getest moet worden]<br>
+            De oorzaak van dit probleem komt voort uit [genuanceerde reden]<br>
+            [screenshot met eventuele voor en na om de oplossing te verduidelijken]<br><br>
+            Alvast bedankt voor de feedback.<br><br>
+            Met vriendelijke groeten,<br>[naam]<br><br>
+            [Intern: Hoe is het opgelost? (bv. Deze patch gebruikt, deze module geupdatet …)]
+        `,
+        TEMPLATE_ESTIMATION_CHANGES: `
+            Beste [naam],<br><br>
+            We hebben je vraag bekeken en stellen het volgende voor:<br>
+                • [Beschrijving van wat er gaat aangepast worden]<br>
+                • [Workflow beschrijven hoe het juist gaat werken]<br>
+                • [FE: Eventueel een screenshot van de aanpassing]<br>
+                • [assumpties (Impact op andere delen van website? Wat niet in scope zit? Op welke rollen dat dit van toepassing is)]<br>
+            We schatten dit in op [uren/dagen] werk.<br><br>
+            Graag je goedkeuring dat we dit zo mogen uitvoeren.<br>
+            Alvast bedankt.<br><br>
+            Met vriendelijke groeten,<br>[naam]<br><br>
+            [Devnote: welke modules gaan gebruikt worden, welke file moet er aangepast worden.]
+        `,
+        TEMPLATE_DELIVERY_CHANGES: `
+            Beste [naam],<br><br>
+            We hebben de gevraagde aanpassing doorgevoerd, die hier gevalideerd kan worden: [link naar expliciete url]<br>
+            Je kan dit testen door<br>
+                • [stappenplan (uitleg hoe er getest moet worden, bv hoe geraak ik er in het menu)]<br>
+                • [Moeten er andere functionaliteiten getest worden?]<br>
+                • [rol waarmee getest moet worden]<br>
+                • [eventueel een voorbeeld geven]<br><br>
+            Alvast bedankt voor de feedback.<br><br>
+            Met vriendelijke groeten,<br>[naam]
+        `,
+        TEMPLATE_QUARTERLY_UPDATES: `
+            Beste [naam],<br><br>
+            We hebben voor verschillende modules een (security) update doorgevoerd. Dit kan gevalideerd worden op de staging omgeving: [url].<br>
+            Hieronder vind je een lijst met de modules die een update hebben ontvangen met een korte beschrijving van de functionaliteit en hoe deze kan worden getest.<br><br>
+                • [Paragraaf versie 2.X.: Deze module zorgt ervoor dat je verschillende content elementen kan toevoegen aan een pagina. Dit kan je testen via …<br>
+                • Rabbit hole: Deze module zorgt ervoor dat bepaalde pagina’s voor gebruikers niet toegankelijk zijn.<br>
+                • Module X. Deze module …. Dit kan je niet echt testen, aangezien dit onderliggend is.]<br>
+            [Link naar algemene uitleg om Drupal core etc te kunnen testen.]<br><br>
+            We voorzien dit over 2 weken naar de productie omgeving te deployen. Gelieve in tussentijd de nodige testen op staging door te voeren zodat dit probleemloos kan verlopen.<br><br>
+            Alvast bedankt.<br><br>
             Met vriendelijke groeten,<br>[naam]
         `
     };
@@ -224,11 +263,38 @@ chrome.runtime.onInstalled.addListener(({ reason }) => {
 
         chrome.contextMenus.create({
             parentId: 'descriptions',
-            title: 'Use Description 2',
+            title: 'Use Bugs Template',
             type: 'radio',
-            checked: result.descriptionType === 'DESCRIPTION_2',
+            checked: result.descriptionType === 'TEMPLATE_BUGS',
             contexts: ['action'],
-            id: 'description_2'
+            id: 'template_bugs'
+        });
+
+        chrome.contextMenus.create({
+            parentId: 'descriptions',
+            title: 'Use Estimation Changes Template',
+            type: 'radio',
+            checked: result.descriptionType === 'TEMPLATE_ESTIMATION_CHANGES',
+            contexts: ['action'],
+            id: 'template_estimation_changes'
+        });
+
+        chrome.contextMenus.create({
+            parentId: 'descriptions',
+            title: 'Use Delivery Changes Template',
+            type: 'radio',
+            checked: result.descriptionType === 'TEMPLATE_DELIVERY_CHANGES',
+            contexts: ['action'],
+            id: 'template_delivery_changes'
+        });
+
+        chrome.contextMenus.create({
+            parentId: 'descriptions',
+            title: 'Use Quarterly Updates Template',
+            type: 'radio',
+            checked: result.descriptionType === 'TEMPLATE_QUARTERLY_UPDATES',
+            contexts: ['action'],
+            id: 'template_quarterly_updates'
         });
 
         chrome.contextMenus.create({
@@ -277,8 +343,17 @@ chrome.contextMenus.onClicked.addListener(function (info) {
         case 'description_1':
             chrome.storage.sync.set({ descriptionType: 'DESCRIPTION_1' });
             break;
-        case 'description_2':
-            chrome.storage.sync.set({ descriptionType: 'DESCRIPTION_2' });
+        case 'template_bugs':
+            chrome.storage.sync.set({ descriptionType: 'TEMPLATE_BUGS' });
+            break;
+        case 'template_estimation_changes':
+            chrome.storage.sync.set({ descriptionType: 'TEMPLATE_ESTIMATION_CHANGES' });
+            break;
+        case 'template_delivery_changes':
+            chrome.storage.sync.set({ descriptionType: 'TEMPLATE_DELIVERY_CHANGES' });
+            break;
+        case 'template_quarterly_updates':
+            chrome.storage.sync.set({ descriptionType: 'TEMPLATE_QUARTERLY_UPDATES' });
             break;
         case 'getting_started':
             chrome.tabs.create({
